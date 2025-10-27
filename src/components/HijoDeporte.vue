d344970371b8d2b2a7db1fbc8eecfd4d672e7244
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-27 14:14:05 +0100

    comunicación padre hijo

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 style="color:blue">Hijo deporte {{deporte}}</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "HijoDeporte",
+        props: ["deporte"]
+    }
+</script>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 style="color:red">Padre deportes</h1>
+        <ul>
+            <li v-for="deporte in deportes" :key="deporte">
+                <HijoDeporte :deporte="deporte"/>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import HijoDeporte from './HijoDeporte.vue'
+
+    export default{
+        name: "PadreDeportes",
+        components: {
+            HijoDeporte
+        },
+        data(){
+            return{
+                deportes: ["Futbol", "Baloncesto", "Tenis", "Padel"]
+            }
+        }
+    }
+</script>
