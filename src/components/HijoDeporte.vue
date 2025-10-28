afe732f6493ce435bbaaa67fe05e3e66df5622e8
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-28 09:34:13 +0100

    Enviar información del hijo al padre

--- src/components/HijoDeporte.vue
+++ src/components/HijoDeporte.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h1 style="color:blue">Hijo deporte {{deporte}}</h1>
+        <button @click="seleccionarFavorito()" >Marcar favorito</button>
     </div>
 </template>
 
 <script>
     export default{
         name: "HijoDeporte",
-        props: ["deporte"]
+        props: ["deporte"],
+        methods: {
+            seleccionarFavorito(){
+                console.log("hijo" + this.deporte);
+                this.$emit("SeleccionarFavoritoParent", this.deporte)
+            }
+        }
     }
 </script>
--- src/components/PadreDeportes.vue
+++ src/components/PadreDeportes.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
         <h1 style="color:red">Padre deportes</h1>
+        <h2 v-if="favorito != null" style="backgroundColor: yellow;"> El deporte favorito es {{favorito}}</h2>
         <ul>
             <li v-for="deporte in deportes" :key="deporte">
-                <HijoDeporte :deporte="deporte"/>
+                <HijoDeporte :deporte="deporte" v-on:seleccionarFavoritoParent="seleccionarFavoritoParent"/>
             </li>
         </ul>
     </div>
@@ -19,7 +20,14 @@ import HijoDeporte from './HijoDeporte.vue'
         },
         data(){
             return{
-                deportes: ["Futbol", "Baloncesto", "Tenis", "Padel"]
+                deportes: ["Futbol", "Baloncesto", "Tenis", "Padel"],
+                favorito: null
+            }
+        },
+        methods: {
+            seleccionarFavoritoParent(deporte){
+                console.log("padre")
+                this.favorito = deporte
             }
         }
     }
