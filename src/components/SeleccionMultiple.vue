0f6e6e2ae8cc35b756be084fefc530209e40c2ca
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-29 18:39:12 +0100

    select multiple finalizado

--- src/components/SeleccionMultiple.vue
+++ src/components/SeleccionMultiple.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-        <form v-on:submit.prevent="cogerValores()">
-            <select multiple>
-                <option v-for="elemento in elementos" :key="elemento">
-                    {{elemento}}
-                </option>
-            </select>
+        <form v-on:submit.prevent="mostrarSeleccionados">
+            <select multiple size="7" ref="selectmultiple">
+                <option v-for="option in options" :key="option" :value="option">{{option}}</option>
+            </select><br>
             <button>Mostrar seleccionados</button>
         </form>
+        <h3 style="color: blue">{{seleccionados}}</h3>
     </div>
 </template>
 
@@ -15,13 +14,20 @@
     export default{
         name: "SeleccionMultiple",
         data () {
-            return{
-                element: [],
-                elementos: ["Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 5", "Elemento 6"]
+            return {
+                options: ["Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 5", "Elemento 6", "Elemento 7"],
+                seleccionados: ""
             }
-        },
-        cogerValores(){
-
+        }, 
+        methods: {
+            mostrarSeleccionados(){
+                for(let option of this.$refs.selectmultiple){
+                    if(option.selected){
+                        this.seleccionados += option.value + ", ";
+                    }
+                }
+            }
         }
+        
     }
 </script>
